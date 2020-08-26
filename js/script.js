4ac8d588c1742ae945ba2e5e23b41375c07b7202
Paul Onyekwelu <silanka@CEO-MacBook-Pro.local>
2020-08-26 14:09:26 -0700

    designing of login ui

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let loggedIn = false;
+
+let users = [
+    {email: 'tony@gmail.com', password: '12345'},
+    {email: 'tolu@gmail.com', password: '12345'},
+    {email: 'amaka@gmail.com', password: '12345'},
+    {email: 'Amina@gmail.com', password: '12345'},
+]
+
+if(log)
